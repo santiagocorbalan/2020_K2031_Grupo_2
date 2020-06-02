6eabdf68eb53b7122e7e3b6330d525aa57eea5b5
Morena Sisro <morenasisro@hotmail.com>
2020-06-02 11:31:54 -0300

    Alineación de código

--- TP2/Tp2_Grupo_2.c
+++ TP2/Tp2_Grupo_2.c
@@ -8,91 +8,86 @@ struct nodo {
   struct nodo *siguiente;
 };
 
-
-void push(char caracter);  
-char pop(void); 
-int ultimo(void); 
-bool vacia(void);  
+void push(char caracter);
+char pop(void);
+int ultimo(void);
+bool vacia(void);
 struct nodo *superior = NULL;
-
 int determinar_columna(char a);
 
+int main() {
 
-int main()
-
-{
+    struct est {
+        int estado;
+        char cad_push[3];
+    };
     
-
-    struct loc { 
-        int estado; 
-        char cad_push[3]; 
-        };  
+    struct est TT[5][5][6];
     
-    struct loc TT[5][5][6];
     TT [0] [0] [0].estado = 3;
     strcpy(TT[0][0][0].cad_push, "");
-    TT [0] [0] [1].estado = 1; 
+    TT [0] [0] [1].estado = 1;
     strcpy(TT[0][0][1].cad_push, "$");
-    TT [0] [0] [2].estado = 3; 
+    TT [0] [0] [2].estado = 3;
     strcpy(TT[0][0][2].cad_push, "");
-    TT [0] [0] [3].estado = 0; 
+    TT [0] [0] [3].estado = 0;
     strcpy(TT[0][0][3].cad_push, "R$");
-    TT [0] [0] [4].estado = 3; 
+    TT [0] [0] [4].estado = 3;
     strcpy(TT[0][0][4].cad_push, "");
-    TT [1] [0] [0].estado = 1; 
+    TT [1] [0] [0].estado = 1;
     strcpy(TT[1][0][0].cad_push, "$");
-    TT [1] [0] [1].estado = 1; 
+    TT [1] [0] [1].estado = 1;
     strcpy(TT[1][0][1].cad_push, "$");
-    TT [1] [0] [2].estado = 0; 
+    TT [1] [0] [2].estado = 0;
     strcpy(TT[1][0][2].cad_push, "$");
-    TT [1] [0] [3].estado = 3; 
+    TT [1] [0] [3].estado = 3;
     strcpy(TT[1][0][3].cad_push, "");
-    TT [1] [0] [4].estado = 3;     
+    TT [1] [0] [4].estado = 3;
     strcpy(TT[1][0][4].cad_push, "");
-    TT [0] [1] [0].estado = 3; 
+    TT [0] [1] [0].estado = 3;
     strcpy(TT[0][1][0].cad_push, "");
-    TT [0] [1] [1].estado = 1; 
+    TT [0] [1] [1].estado = 1;
     strcpy(TT[0][1][1].cad_push, "R");
-    TT [0] [1] [2].estado = 3; 
+    TT [0] [1] [2].estado = 3;
     strcpy(TT[0][1][2].cad_push, "");
-    TT [0] [1] [3].estado = 0; 
+    TT [0] [1] [3].estado = 0;
     strcpy(TT[0][1][3].cad_push, "RR");
-    TT [0] [1] [4].estado = 3; 
+    TT [0] [1] [4].estado = 3;
     strcpy(TT[0][1][4].cad_push, "");
-    TT [1] [1] [0].estado = 1; 
+    TT [1] [1] [0].estado = 1;
     strcpy(TT[1][1][0].cad_push, "R");
-    TT [1] [1] [1].estado = 1; 
+    TT [1] [1] [1].estado = 1;
     strcpy(TT[1][1][1].cad_push, "R");
-    TT [1] [1] [2].estado = 0; 
+    TT [1] [1] [2].estado = 0;
     strcpy(TT[1][1][2].cad_push, "R");
-    TT [1] [1] [3].estado = 3; 
+    TT [1] [1] [3].estado = 3;
     strcpy(TT[1][1][3].cad_push, "");
-    TT [1] [1] [4].estado = 2; 
+    TT [1] [1] [4].estado = 2;
     strcpy(TT[1][1][4].cad_push, "E");
-    TT [2] [1] [0].estado = 3; 
+    TT [2] [1] [0].estado = 3;
     strcpy(TT[2][1][0].cad_push, "");
-    TT [2] [1] [1].estado = 3; 
+    TT [2] [1] [1].estado = 3;
     strcpy(TT[2][1][1].cad_push, "");
-    TT [2] [1] [2].estado = 0; 
+    TT [2] [1] [2].estado = 0;
     strcpy(TT[2][1][2].cad_push, "R");
-    TT [2] [1] [3].estado = 3; 
+    TT [2] [1] [3].estado = 3;
     strcpy(TT[2][1][3].cad_push, "");
-    TT [2] [1] [4].estado = 2; 
+    TT [2] [1] [4].estado = 2;
     strcpy(TT[2][1][4].cad_push, "E");
-    TT [2] [0] [0].estado = 3; 
+    TT [2] [0] [0].estado = 3;
     strcpy(TT[2][0][0].cad_push, "");
-    TT [2] [0] [1].estado = 3; 
+    TT [2] [0] [1].estado = 3;
     strcpy(TT[2][0][1].cad_push, "");
-    TT [2] [0] [2].estado = 0; 
+    TT [2] [0] [2].estado = 0;
     strcpy(TT[2][0][2].cad_push, "$");
-    TT [2] [0] [3].estado = 3; 
+    TT [2] [0] [3].estado = 3;
     strcpy(TT[2][0][3].cad_push, "");
-    TT [2] [0] [4].estado = 3; 
+    TT [2] [0] [4].estado = 3;
     strcpy(TT[2][0][4].cad_push, "");
     
     int estado_actual = 0;
-    int b = 0 ;
-    int i=0;
+    int b = 0;
+    int i = 0;
     char operr[3];
     
     char caracter1 = '$';
@@ -101,72 +96,61 @@ int main()
     
     char operacion [128];
 
-
     printf("Ingrese la operacion: ");
     scanf("%s" , operacion);
     char caracter = operacion[i];
     
-     while (caracter != '\0'){
+    while (caracter != '\0'){
         
-           b = TT[estado_actual] [ultimo()] [determinar_columna(caracter)].estado ;
-           strcpy(operr,TT[estado_actual] [ultimo()] [determinar_columna(caracter)].cad_push);
+        b = TT[estado_actual] [ultimo()] [determinar_columna(caracter)].estado ;
+        strcpy(operr,TT[estado_actual] [ultimo()] [determinar_columna(caracter)].cad_push);
 
-          if (b == 3){
-              
-             printf("Syntax Error.\n");
-              
+        if (b == 3) {
+            printf("Syntax Error.\n");
              return 0;
-            }
+        }
 
-          if (caracter == '('){
-             char val = pop();
-             push(val);
-             push(operr[0]);
-            }
+        if (caracter == '(') {
+            char val = pop();
+            push(val);
+            push(operr[0]);
+        }
             
-         if (caracter == ')'){
-                pop();
-            }
-            
-        
+        if (caracter == ')') {
+            pop();
+        }
+    
         estado_actual = b;
         i++;
-        caracter =operacion[i];
+        caracter = operacion[i];
     }
 
-       pop();
+    pop();
                
-        if (vacia()) {
+    if (vacia()) {
             
-            if (estado_actual != 0 ) {
-        printf("La Operacion Ingresada Es Correcta.\n");
-            }
-            else {
+        if (estado_actual != 0 ) {
+            printf("La Operacion Ingresada Es Correcta.\n");
+        } else {
+            printf("Syntax Error.\n");
+        }
+        
+    } else {
         printf("Syntax Error.\n");
-      }
-      } else {
-        printf("Syntax Error.\n");
-      }
-                    
-
-
+    }
+    
     return 0;
 }
 
-
-int determinar_columna(char a)
-{
+int determinar_columna(char a) {
+    
     int col = 0;
-    switch(a)
-
-    {
-
-      case '0': col=0; break;
-      case '1'...'9': col=1; break;
-      case '+': case '-': case '*': case '/':  col=2; break;
-      case '(': col=3; break;
-      case ')': col=4; break;
-
+    switch (a) {
+        case '0': col=0; break;
+        case '1'...'9': col=1; break;
+        case '+': case '-': case '*': case '/':  col=2; break;
+        case '(': col=3; break;
+        case ')': col=4; break;
     }
     return col;
 }
@@ -174,7 +158,6 @@ int determinar_columna(char a)
 char pop(void) {
     
     if (superior != NULL) {
-
         struct nodo *temporal = superior;
         char valor = superior->dato;
         superior = superior->siguiente;
@@ -184,7 +167,6 @@ char pop(void) {
     }
 }
 
-
 void push(char caracter) {
 
   struct nodo *nuevoNodo = malloc(sizeof(struct nodo));
@@ -193,18 +175,18 @@ void push(char caracter) {
   superior = nuevoNodo;
 }
 
-
-
 int ultimo() {
-              
-         if (superior-> dato == '$'){
-                   return 0 ;
-            }
+    
+    if (superior-> dato == '$'){
+        return 0;
+    }
             
-             if (superior-> dato == 'R'){
-                 return 1 ;
-            }
+    if (superior-> dato == 'R'){
+        return 1;
+    }
 
-          }
+}
 
-bool vacia(void) { return superior == NULL; }
+bool vacia(void) {
+    return superior == NULL;
+}
