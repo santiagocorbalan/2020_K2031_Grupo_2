537a13198d084c1f9d35ce707a6c7ce3deeab4e0
santiagocorbalan <santiago.corbalan@hotmail.com>
2020-11-24 13:44:48 -0300

    Final

--- TP Integrador/docDePrueba.c
+++ TP Integrador/docDePrueba.c
@@ -4,11 +4,11 @@ int funcionSinParametros(); // funcion sin parametros
 
 int a =20; // declaracion de variable correctamente
 
-char a,b,c ; // doble declaracion de a 
+char a,b,c ; // doble declaracion de a  -- Error semantico
 
 x = funcion(); // "x" nunca fue declarada! 
 
-int 20; // error de declaracion -> tiene que ser un IDENTIFICADOR
+int 20; // error de declaracion -> tiene que ser un IDENTIFICADOR -- Error Sintactico
 
 
 if (valorA >= valorB) {}; // sentencia IF 
@@ -17,4 +17,4 @@ while (i != 0) {}; // sentencia WHILE
 
 return ; // Sentencia de Salto
 
-ç
+ç // ?? -- caracter no reconocido por FLEX -- Error Lexico
